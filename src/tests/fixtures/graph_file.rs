916a64fad3c5fb2cbae354aa56e43aa72afce90e
Raphael <raphaelricardo10@gmail.com>
2022-12-09 20:20:21 +0000

    🦄 refactor: removed dotenv
    removed .env variables and used the system env variables

--- src/tests/fixtures/graph_file.rs
+++ src/tests/fixtures/graph_file.rs
@@ -1,16 +1,10 @@
-use dotenv::dotenv;
 use rstest::fixture;
 
 use crate::file_reader::graph_file_reader::GraphFileReader;
 
 #[fixture]
 pub fn filename() -> String {
-    dotenv().ok();
-
-    format!(
-        "{}/TPI_BC_COL_0.txt",
-        dotenv::var("INSTANCES_PATH").unwrap()
-    )
+    format!("{}/TPI_BC_COL_0.txt", env!("INSTANCES_PATH"))
 }
 
 #[fixture]
