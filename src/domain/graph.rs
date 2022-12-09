e730247f8421fa604c569c74058b56ae4d710924
Raphael <raphaelricardo10@gmail.com>
2022-12-09 07:10:53 +0000

    🦄 refactor: clean code
    added constructor and created some type alias to match with business logic

--- src/domain/graph.rs
+++ src/domain/graph.rs
@@ -1,7 +1,20 @@
 use std::collections::HashSet;
 
+type Edge = (u32, u32);
+type EdgeSet = HashSet<Edge>;
+
 pub(crate) struct Graph {
     pub num_edges: u32,
     pub num_vertex: u32,
-    pub edges: HashSet<(u32, u32)>,
+    pub edges: EdgeSet,
+}
+
+impl Graph {
+    pub(crate) fn new(num_edges: u32, num_vertex: u32, edges: EdgeSet) -> Self {
+        Self {
+            edges,
+            num_edges,
+            num_vertex,
+        }
+    }
 }
