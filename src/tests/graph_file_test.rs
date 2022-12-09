e3ed77467d1d66610ab1b6772a0d69a44b7b9be1
Raphael <raphaelricardo10@gmail.com>
2022-12-09 07:13:30 +0000

    🧪 test: header reader

--- src/tests/graph_file_test.rs
+++ src/tests/graph_file_test.rs
@@ -1,11 +1,30 @@
+use std::env;
+
+use dotenv::dotenv;
 use crate::file_reader::graph_file_reader::GraphFileReader;
 
 #[test]
 fn test_can_read_file() {
     let filename = "../TPI_BC_COL_1.txt";
 
-    let graph = GraphFileReader::new(filename).read_file();
+    let graph = GraphFileReader::new(filename).read_file().unwrap();
 
     assert_eq!(graph.num_vertex, 25);
     assert_eq!(graph.num_edges, 320);
+}
+
+#[test]
+fn test_can_read_csv_header() {
+    dotenv().ok();
+    let filename = format!("{}/TPI_BC_COL_0.txt", dotenv::var("INSTANCES_PATH").unwrap());
+
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .delimiter(b' ')
+        .from_path(filename).unwrap();
+
+    let header = GraphFileReader::read_header(&mut reader);
+
+    assert_eq!(header.num_vertex, 25);
+    assert_eq!(header.num_edges, 320);
 }
