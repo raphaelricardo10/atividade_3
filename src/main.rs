66cf77b46e992f4d3f5ca67e7bc299ca3a92ccad
Raphael <raphaelricardo10@gmail.com>
2022-12-10 22:15:13 +0000

    ✨ feat: added main
    added the logic of main file to solve the graph problem using the lp_solver module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
+use graph_col_solver::file_reader::graph_file_reader::GraphFileReader;
+use graph_col_solver::lp_solver::solver;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = env::args().nth(1).expect("No filename was provided!");
+    println!("Filename: {}", filename);
+
+    let graph = GraphFileReader::new(filename).read_file().unwrap();
+
+    solver::solve_lp_graph_problem(graph).unwrap();
 }
