5bc7450a1efe7d741d8009dcfd68400c6e9269ed
Raphael <raphaelricardo10@gmail.com>
2022-12-11 04:12:08 +0000

    saving broken files

--- src/lp_solver/py_solver/solver.py
+++ src/lp_solver/py_solver/solver.py
@@ -1,2 +1,10 @@
+from docplex.mp.model import Model
+
 def solve_lp_graph_problem(num_vertices, num_edges, edges):
-    pass
+    m = Model(name='Graph Coloring Problem with UserCut')
+    x = m.continuous_var(name="x", lb=0)
+    c1 = m.add_constraint(x >= 2, ctname="const1")
+    m.set_objective("min", 3*x)
+    m.print_information()
+    m.solve()
+    m.print_solution()
